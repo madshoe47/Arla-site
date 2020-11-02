9d0fc2a95bd4c90fa8e3adeadfdf860a52d0c894
madshoe47 <56107464+madshoe47@users.noreply.github.com>
2020-11-03 00:07:03 +0100

    Første del på forsiden - Mads

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,25 @@
 'use strict'
 
-if (rapport === done) {
-    party == on
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
-1
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function rotateFunction() {
+    document.getElementsByClassName("arrowDropdown").style.transform = 'rotate(180deg)';
+}
