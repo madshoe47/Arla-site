f24f23385c6cfea9af4d0635f0104f7b7e6e7ef2
sandrabirkefeldt <56107469+sandrabirkefeldt@users.noreply.github.com>
2020-11-08 22:33:26 +0100

    modal

--- form.js
+++ form.js
@@ -36,3 +36,21 @@ $("#pro-form input").keyup(function () {
     }
 
 });
+
+let modal = document.getElementById("modalID");
+let helpBtn = document.getElementById("helpIconFormID");
+let span =  document.getElementsByClassName("close")[0];
+
+helpBtn.onclick = function () {
+    modal.style.display = "block";
+};
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
